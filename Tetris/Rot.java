14814a9f88ea76a6f44e93ffc7752b23f568b3ae
hfgd <46094961+hfgd123@users.noreply.github.com>
2022-06-03 11:16:20 +0200

    Add files via upload

---
+++ Tetris/Rot.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Rot here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Rot extends Actor
+{
+    /**
+     * Act - do whatever the Rot wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
